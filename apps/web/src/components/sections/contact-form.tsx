b7abd065097ef9c040c7fa2a91e71f8c81ff6000
Gavin Grant <gavingrant@gmail.com>
2025-03-09 22:50:10 -0700

    refactor(contact): spacing

--- apps/web/src/components/sections/contact-form.tsx
+++ apps/web/src/components/sections/contact-form.tsx
@@ -111,77 +111,79 @@ export function ContactForm({ title }: ContactFormProps) {
     "rounded-e-none border-e-0 focus-visible:ring-0 outline-none w-full dark:text-zinc-50 dark:placeholder:text-zinc-500 bg-background items-center border rounded-xl p-6 drop-shadow-lg w-full justify-between";
 
   return (
-    <section
-      id="contact-form"
-      className="relative container mx-auto px-4 md:px-8 py-8 sm:py-16 md:py-20 lg:py-24 bg-zinc-50 dark:bg-zinc-900 rounded-3xl overflow-hidden"
-    >
-      <div className="container mx-auto px-4 md:px-6 flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-4 w-full">
-          <h2 className="mb-4 text-xl font-semibold text-gray-900 dark:text-neutral-300 sm:text-3xl md:text-5xl text-balance">
-            {title}
-          </h2>
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-4 w-full max-w-3xl"
-          >
-            <Input
-              className="hidden"
-              name="honeypot"
-              value={formData.honeypot}
-              onChange={handleChange}
-            />
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    <div className="relative w-full flex items-center justify-center">
+      <section
+        id="contact-form"
+        className="relative container w-full spx-4 md:px-8 py-8 sm:py-16 md:py-20 lg:py-24 bg-zinc-50 dark:bg-zinc-900 rounded-3xl overflow-hidden mx-4"
+      >
+        <div className="container px-4 flex items-center justify-center w-full">
+          <div className="flex flex-col items-center justify-center gap-4 w-full">
+            <h2 className="mb-4 text-xl font-semibold text-gray-900 dark:text-neutral-300 sm:text-3xl md:text-5xl text-balance">
+              {title}
+            </h2>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-4 w-full max-w-3xl"
+            >
+              <Input
+                className="hidden"
+                name="honeypot"
+                value={formData.honeypot}
+                onChange={handleChange}
+              />
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                <Input
+                  className={inputClasses}
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+                <Input
+                  className={inputClasses}
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+              </div>
               <Input
                 className={inputClasses}
-                type="text"
-                name="firstName"
-                placeholder="First Name"
-                value={formData.firstName}
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                value={formData.email}
                 onChange={handleChange}
               />
               <Input
                 className={inputClasses}
                 type="text"
-                name="lastName"
-                placeholder="Last Name"
-                value={formData.lastName}
+                name="message"
+                placeholder="Message"
+                value={formData.message}
                 onChange={handleChange}
               />
-            </div>
-            <Input
-              className={inputClasses}
-              type="email"
-              name="email"
-              placeholder="Email Address"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <Input
-              className={inputClasses}
-              type="text"
-              name="message"
-              placeholder="Message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-            <div className="flex items-center justify-center w-full mt-6">
-              <Button type="submit" disabled={isLoading}>
-                {isLoading ? "Sending..." : "Send Message"}
-              </Button>
-            </div>
-            {status.message && (
-              <p
-                className={`text-center text-sm ${
-                  status.success ? "text-green-600" : "text-red-600"
-                }`}
-              >
-                {status.message}
-              </p>
-            )}
-          </form>
+              <div className="flex items-center justify-center w-full mt-6">
+                <Button type="submit" disabled={isLoading}>
+                  {isLoading ? "Sending..." : "Send Message"}
+                </Button>
+              </div>
+              {status.message && (
+                <p
+                  className={`text-center text-sm ${
+                    status.success ? "text-green-600" : "text-red-600"
+                  }`}
+                >
+                  {status.message}
+                </p>
+              )}
+            </form>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
 
--- apps/web/src/components/sections/properties.tsx
+++ apps/web/src/components/sections/properties.tsx
@@ -8,6 +8,7 @@ export function Properties({ title }: PropertiesProps) {
       <div className="container mx-auto px-4 md:px-6 space-y-6 text-center">
         <h1 className="text-4xl lg:text-5xl font-semibold">{title}</h1>
         <iframe
+          className="rounded-3xl"
           src="https://www.compass.com/c/mark-mendez/architecture-for-sale?agent_id=5791082452f6eb36de5090f3"
           width="100%"
           height="1600px"
