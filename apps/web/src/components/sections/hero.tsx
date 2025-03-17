759de00ec5762ba2e33f343c2da36d98a28e5f08
Gavin Grant <gavingrant@gmail.com>
2025-03-16 20:26:14 -0700

    refactor(navbar): z-index

--- apps/web/src/components/sections/hero.tsx
+++ apps/web/src/components/sections/hero.tsx
@@ -16,7 +16,7 @@ export function HeroBlock({
   richText,
 }: HeroBlockProps) {
   return (
-    <section id="hero" className="mt-4 md:my-16">
+    <section id="hero" className="mt-4 md:my-10">
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex items-center flex-col lg:flex-row gap-6 lg:gap-20">
           <div className="grid h-full grid-rows-[auto_1fr_auto] gap-4 lg:gap-6 items-center justify-items-center text-center lg:items-start lg:justify-items-start lg:text-left">
